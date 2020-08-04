6320ebc6be5156d31bc4a1c9a440b07551d56a71
radovan25513 <48982803+radovan25513@users.noreply.github.com>
2020-08-04 14:22:04 +0200

    Spring initializr

---
+++ src/main/java/com/americavsrussia/americavsrussia/AmericavsrussiaApplication.java
@@ -0,0 +1,13 @@
+package com.americavsrussia.americavsrussia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmericavsrussiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmericavsrussiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/americavsrussia/americavsrussia/AmericavsrussiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.americavsrussia.americavsrussia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmericavsrussiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
